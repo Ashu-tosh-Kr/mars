f0239214fb6d58ab9f5122d96778fdf4960d975b
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2021-12-04 09:53:49 +0530

    Sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,33 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import React from "react";
 
 import LoginScreen from "screens/LoginScreen";
+import Sidebar from "components/globals/Sidebar";
+import Dashboard from "screens/Dashboard";
+import { Flex } from "@chakra-ui/layout";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LoginScreen />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/*" element={<MainRoutes />}>
+          <Route path="home" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
+
+const MainRoutes = () => {
+  return (
+    <Flex justify="flex-start" pos="relative">
+      <Sidebar />
+      <Flex w="80%" m="2rem" p="2rem">
+        <Outlet />
+      </Flex>
+    </Flex>
+  );
+};
 
 export default App;
---
+++ client/src/screens/Dashboard.jsx
@@ -0,0 +1,7 @@
+import { Box } from "@chakra-ui/layout";
+
+const Dashboard = () => {
+  return <Box></Box>;
+};
+
+export default Dashboard;
