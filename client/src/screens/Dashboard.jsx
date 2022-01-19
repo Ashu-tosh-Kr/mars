d963d512b2def88624df22a82c91131740578d4d
user <email@email.com>
2022-01-19 21:53:35 +0900

    Added TODO comment on locale

--- client/src/screens/Dashboard.jsx
+++ client/src/screens/Dashboard.jsx
@@ -1,9 +1,10 @@
-import { Box } from "@chakra-ui/layout";
+import { Box } from '@chakra-ui/layout';
 
 const Dashboard = () => {
   return (
     <Box>
       <iframe
+        // TODO 1. ↓ ctz shall be Japan for now - or, what if we get rid of the ctz param itself?
         src="https://calendar.google.com/calendar/embed?src=test123sbjct%40gmail.com&ctz=Asia%2FKolkata"
         style={{ border: 0 }}
         width="1200"
