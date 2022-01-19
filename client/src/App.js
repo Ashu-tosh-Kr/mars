2422ef9fb82401f40c8e115caae634ebb95596b3
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2022-01-20 00:08:44 +0530

    ui disruption fix due to extra button

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import React from "react";
-
+//non lib imports
+import "utils/i18n";
 import LoginScreen from "screens/LoginScreen";
-
 import Dashboard from "screens/Dashboard";
 import ClientCompanyScreen from "screens/ClientCompanyScreen";
 import PageNotFound from "screens/errorPages/PageNotFound";
@@ -13,14 +13,10 @@ import NewGigScreen from "screens/NewGigScreen";
 import TodoScreen from "screens/TodoScreen";
 import AllGigsScreen from "screens/AllGigsScreen";
 import SettingsScreen from "screens/SettingsScreen";
-import Translator from "./components/Translator";
 
 function App() {
   return (
     <>
-      {/* TODO ↓ is this the best place? */}
-      <Translator />
-
       <Routes>
         <Route path="/login" element={<LoginScreen />} />
         <Route path="/*" element={<RequireAuth />}>
