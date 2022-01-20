a96e9d930a59527b6d788622f4b6d54cb004e6ac
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2022-01-20 20:31:07 +0530

    minor dashboard changes

--- client/src/screens/Dashboard.jsx
+++ client/src/screens/Dashboard.jsx
@@ -1,18 +1,46 @@
-import { Box } from '@chakra-ui/layout';
+import { HStack, Box } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/react";
+import { FcTodoList } from "react-icons/fc";
+
+//non lib imports
+import { useGetCurrUserInfo } from "api/hooks";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  /**
+   * hooks
+   */
+  const navigate = useNavigate();
+  //queries
+  const { userInfo } = useGetCurrUserInfo();
+
+  //jsx
   return (
-    <Box>
-      <iframe
-        // TODO 1. ↓ ctz shall be Japan for now - or, what if we get rid of the ctz param itself?
-        src="https://calendar.google.com/calendar/embed?src=test123sbjct%40gmail.com&ctz=Asia%2FKolkata"
-        style={{ border: 0 }}
-        width="1200"
-        height="600"
-        frameBorder="0"
-        scrolling="no"
-      ></iframe>
-    </Box>
+    <HStack spacing={"4rem"}>
+      <Button
+        onClick={() => navigate("/todo", { replace: true })}
+        height="48px"
+        width="200px"
+        fontSize="2rem"
+        leftIcon={<FcTodoList />}
+        colorScheme="teal"
+        variant="ghost"
+      >
+        Todos: {userInfo?.todos?.length}
+      </Button>
+
+      <Box>
+        <iframe
+          // TODO 1. ↓ ctz shall be Japan for now - or, what if we get rid of the ctz param itself?
+          src="https://calendar.google.com/calendar/embed?src=test123sbjct%40gmail.com&ctz=Asia%2FKolkata"
+          style={{ border: 0 }}
+          width="900"
+          height="600"
+          frameBorder="0"
+          scrolling="no"
+        ></iframe>
+      </Box>
+    </HStack>
   );
 };
 
